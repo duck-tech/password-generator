c02706b0f1ce906da6a9df73a45d63238440da63
duck-tech <babymis0628@gmail.com>
2021-10-30 23:56:20 +0800

    password_generator

---
+++ app.js
@@ -0,0 +1,30 @@
+// require related modules used in the project
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generatePassword = require('./generate_password')
+const app = express()
+const port = 3000
+
+// set template engine in express
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/', (req, res) => {
+    const options = req.body
+    const password = generatePassword(options)
+    res.render('index', { password, options })
+})
+
+// start the express server and listening for connections.
+app.listen(port, () => {
+    console.log(`The Express server is running on http://localhost:${port}`)
+})
